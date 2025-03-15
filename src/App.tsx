5da83985cff75ad0ba23eeab4c528f1a5cb568ed
Gonzalo <gska2010@gmail.com>
2025-03-14 21:10:47 -0500

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from './Button';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [contador, setContador] = useState<number>(0);
+      
+  const handleClickFirst = () =>{
+    console.log(1);
+  };
+  const handleClickSecond= () =>{
+    console.log(2);
+  };
+  const handleClickThird = () =>{
+    console.log(3);
+  };
 
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Haz hecho click {contador} veces</p>
+      <button onClick={() => setContador(contador+1)}>Incrementar</button>
+
+    <section>
+        <h1>Lista de botones</h1>
+        <Button label="Boton 1" handleClick={handleClickFirst}/>
+        <Button handleClick={handleClickSecond}/>
+        <Button label="Boton 3" handleClick={handleClickThird}/>
+    </section>
     </>
-  )
+  );
 }
 
 export default App
